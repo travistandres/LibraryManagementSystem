4e3af0657cac2f8c09fcb2a0031912c9e1cdc7ec
JElwellMSU <jelwell@murraystate.edu>
2023-10-24 15:25:33 -0400

    AddUser window now displays correctly

--- src/AddUser.java
+++ src/AddUser.java
@@ -7,47 +7,50 @@ import java.awt.event.ActionListener;
 
 public class AddUser 
 {
-    // Logic for the add user button
-    // Opens a popup window when the add user button is pressed
-    public static void AddUser(JFrame parent)
-    {
-        // Create a new JFrame to hold the popup window
+    public static void AddUser(JFrame parent) {
+        // Create a new JDialog to hold the popup window
         JDialog popup = new JDialog(parent, "Add User", true);
-        popup.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        popup.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         popup.setSize(446, 326);
         popup.setLocationRelativeTo(null);
-        popup.setVisible(true);
         popup.setResizable(false);
 
-       // Create a new JPanel to hold the text fields and button
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        // Create a new JPanel to hold the text fields and button
+        JPanel panel = new JPanel(new GridLayout(0, 2, 10, 10)); // GridLayout to arrange components in rows and columns
         panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // Create labels and text fields for user input
+        JLabel firstNameLabel = new JLabel("First Name:");
+        JTextField firstName = new JTextField();
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        JTextField lastName = new JTextField();
+        JLabel addressLabel = new JLabel("Address:");
+        JTextField address = new JTextField();
+
+        // Create the close button
+        JButton closeButton = new JButton("Close");
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                popup.dispose(); // Close the popup
+            }
+        });
+
+        // Add components to the panel
+        panel.add(firstNameLabel);
+        panel.add(firstName);
+        panel.add(lastNameLabel);
+        panel.add(lastName);
+        panel.add(addressLabel);
+        panel.add(address);
+        panel.add(new JLabel()); // Empty label for spacing
+        panel.add(closeButton);
+
+        // Add the panel to the popup
         popup.add(panel);
 
-        // Create a new JLabel for the first name text field
-        JLabel firstNameLabel = new JLabel("First Name:");
-        panel.add(firstNameLabel);
-
-        // Create a new JTextField for the user to enter the first name
-        JTextField firstName = new JTextField();
-        panel.add(firstName);
-
-        // Create a new JLabel for the last name text field
-        JLabel lastNameLabel = new JLabel("Last Name:");
-        panel.add(lastNameLabel);
-
-        // Create a new JTextField for the user to enter the last name
-        JTextField lastName = new JTextField();
-        panel.add(lastName);
-
-        // Create a new JLabel for the address text field
-        JLabel addressLabel = new JLabel("Address:");
-        panel.add(addressLabel);
-
-        // Create a new JTextField for the user to enter the address
-        JTextField address = new JTextField();
-        panel.add(address);
+        // Make the popup visible after adding components
+        popup.setVisible(true);
     }
 
 }
