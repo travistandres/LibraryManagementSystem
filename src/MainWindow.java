0a0bb47dcba1b9c6a4388a27cfc34c8fc9ec69c9
JElwellMSU <jelwell@murraystate.edu>
2023-10-30 15:42:11 -0400

    Cleared Bin

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,12 +6,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 
 public class MainWindow {
   JFrame frame;
