e789e05971b4834e2bc2eb959d666bdf47ec9103
Dennon <todian24@gmail.com>
2023-11-03 12:30:01 -0500

    DW 11/3/2023 just added a comment

--- src/Searching.java
+++ src/Searching.java
@@ -7,7 +7,9 @@ import javax.swing.RowFilter;
 
 public class Searching {
     //Referenced http://www.java2s.com/Tutorials/Java/Swing/JTable/Filter_a_JTable_with_RowFilter_in_Java.htm
+    //search looks through all columns of a table and matches the data in the row to the search text to see if it should be shown
     public void search(String searchFor, DefaultTableModel model, JTable table){
+        
         RowFilter<Object, Object> filter = new RowFilter<Object,Object>() {
             public boolean include(Entry entry) {
                 boolean hasMatch = false;
