f5ad694af37cbd111740de0ba83b462c34aed8f1
Dennon <todian24@gmail.com>
2023-11-15 15:38:54 -0600

    slight adjustment

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -383,37 +383,35 @@ public class MainWindow {
         // if User Table is in focus, pops up a confirmation for removing selected user
         int userSelectedRow = userTable.getSelectedRow();
         // DW 11/15 check if selected user has books out before deletion
-        if (!user.hasBookOut(Integer.valueOf((String) userTable.getModel().getValueAt(userSelectedRow, 0)))){
-          if (userSelectedRow >= 0) {
-            String message = "Are you sure you want to remove \""
-                + userTable.getModel().getValueAt(userSelectedRow, 1)
-                + "\"?";
-            int reply = JOptionPane.showConfirmDialog(null, message, "Remove User", JOptionPane.YES_NO_OPTION);
-            if (reply == JOptionPane.YES_OPTION) {
-              String message1 = "Are you REALLY sure you want to remove \""
-                + userTable.getModel().getValueAt(userSelectedRow, 1)
-                + "\"? (This action cannot be undone)";
-              int reply1 = JOptionPane.showConfirmDialog(null, message1, "Remove User", JOptionPane.YES_NO_OPTION);
-              if (reply1 == JOptionPane.YES_OPTION) {
-                // Deletes the data in the database
-                int user_ID = Integer.valueOf((String) userTable.getModel().getValueAt(userSelectedRow, 0));
-                try {
-                  user.deleteUser(user_ID);
-                } catch (Exception e1) {
-                  JOptionPane.showMessageDialog(null, "Failed to remove, please try again.");
-                  e1.printStackTrace();
-                }
-                userModel.removeRow(userSelectedRow);
-                JOptionPane.showMessageDialog(null, "User removed");
-              } else {
-                JOptionPane.showMessageDialog(null, "Removal of user canceled.");
+        if (userSelectedRow >= 0 && !user.hasBookOut(Integer.valueOf((String) userTable.getModel().getValueAt(userSelectedRow, 0)))) {
+          String message = "Are you sure you want to remove \""
+              + userTable.getModel().getValueAt(userSelectedRow, 1)
+              + "\"?";
+          int reply = JOptionPane.showConfirmDialog(null, message, "Remove User", JOptionPane.YES_NO_OPTION);
+          if (reply == JOptionPane.YES_OPTION) {
+            String message1 = "Are you REALLY sure you want to remove \""
+              + userTable.getModel().getValueAt(userSelectedRow, 1)
+              + "\"? (This action cannot be undone)";
+            int reply1 = JOptionPane.showConfirmDialog(null, message1, "Remove User", JOptionPane.YES_NO_OPTION);
+            if (reply1 == JOptionPane.YES_OPTION) {
+              // Deletes the data in the database
+              int user_ID = Integer.valueOf((String) userTable.getModel().getValueAt(userSelectedRow, 0));
+              try {
+                user.deleteUser(user_ID);
+              } catch (Exception e1) {
+                JOptionPane.showMessageDialog(null, "Failed to remove, please try again.");
+                e1.printStackTrace();
               }
+              userModel.removeRow(userSelectedRow);
+              JOptionPane.showMessageDialog(null, "User removed");
             } else {
               JOptionPane.showMessageDialog(null, "Removal of user canceled.");
             }
+          } else {
+            JOptionPane.showMessageDialog(null, "Removal of user canceled.");
           }
-        } else {
-          JOptionPane.showMessageDialog(null, "User has books out! This user cannot be deleted until all their books are checked-in");
+        }else {
+        JOptionPane.showMessageDialog(null, "User has books out! This user cannot be deleted until all their books are checked-in");
         }
       }
     });
